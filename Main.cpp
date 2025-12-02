2a2858f7a12f1005079c1d0ab6bb190b3772d371
eli <anaelicelayam@gmail.com>
2025-12-02 09:11:57 -0600

    menu cute

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,7 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
-#include "Transaction.h"
-#include "Category.h"
+#include "Carro.h"
 #include "Analyzer.h"
 
 using namespace std;
@@ -26,59 +25,38 @@ int main()
 
     do
     {
-        cout << "\n--- MENU ---\n"
+        cout << "\n─ ⊹ ⊱꒰ MENU ꒱⊰ ⊹ ─\n"
              << endl;
         cout << "  ______\n"
              << " /|_||_ |.__\n"
              << "(   _    _ _|\n"
              << "=`-(_)--(_)- \n";
-        cout << "1. Mostrar todos los carros\n";
-        cout << "2. Filtrar carros por fechas\n";
-        cout << "3. Carros con bajos kilometrajes\n";
-        cout << "4. Quiz de carro ideal\n";
-        cout << "0. Salir\n";
-        cout << "Opcion: ";
+        cout << "1 ⋆ Show all cars\n";
+        cout << "2 ⋆ Filter cars by dates\n";
+        cout << "3 ⋆ Show cars with low mileage\n";
+        cout << "4 ⋆ Ideal car quiz\n";
+        cout << "0 ⋆ Exit\n";
+        cout << "Option: ";
+
         cin >> opcion;
 
         if (opcion == 1)
         {
-            analyzer.printTransactions();
+            cout << "All cars\n" << "· · ─ ·ʚɞ· ─ · ·" << endl;
         }
         else if (opcion == 2)
         {
-            string ini, fin;
-            cout << "Start date (YYYY): ";
-            cin >> ini;
-            cout << "End date (YYYY): ";
-            cin >> fin;
-            try
-            {
-                auto y1 = analyzer.parseDate(ini);
-                auto y2 = analyzer.parseDate(fin);
-
-                auto filtradas = analyzer.filtrarRangoFechas(y1, y2);
-
-                for (auto &tx : filtradas)
-                {
-                    cout << tx << endl;
-                }
-            }
-            catch (const exception &e)
-            {
-                cerr << "Error al filtrar: " << e.what() << endl;
-            }
+            cout << "Cars by dates\n" << "· · ─ ·ʚɞ· ─ · ·" << endl;
         }
         else if (opcion == 3)
         {
-            analyzer.getTopDay();
-            analyzer.getTopStore();
-            analyzer.getTopCategory();
+            cout << "Cars with low mileage\n" << "· · ─ ·ʚɞ· ─ · ·" << endl;
         }
         else if (opcion == 4)
         {
-            // aqui va el metodo para el quiz
+            cout << "Ideal car quiz\n" << "· · ─ ·ʚɞ· ─ · ·" << endl;
         }
     } while (opcion != 0);
-
+    cout << "Bye bye! ⊹ ₊ ⟡ ⋆ .\n" << endl;
     return 0;
 }
